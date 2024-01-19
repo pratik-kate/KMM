2353068e2c4ba1a7ef22954f9fdc1dc57e8828fc
Pratik Kate <pratik.kate@dista.ai>
2024-01-19 18:45:29 +0530

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,10 @@
+import codes.pratik.kmmlibrary.MainSerializer;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        MainSerializer serializer = new MainSerializer();
+        serializer.serialize();
+    }
+}
